b3442ba2d5c639ecee90964eedb191fe075e57cf
Liam Yates <liamyatesnz@gmail.com>
2023-01-24 23:33:43 +1300

    Fix still being able to type in user name textbox after pressing enter

--- Game/Assets/Scripts/CreationController.cs
+++ Game/Assets/Scripts/CreationController.cs
@@ -10,14 +10,6 @@ public class CreationController : MonoBehaviour
     public InputField inputField;
     public string userName;
 
-    public float interval = 100;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -26,14 +18,14 @@ public class CreationController : MonoBehaviour
     }
 
     //Creates character, saves name and current level to disc using SaveManager class which doesn't destroy upon changing scenes
-
     public void createCharacter()
     {
+        inputField.enabled = false;
         string name = inputField.text;
         if (name.Length > 0)
         {
             userName = name;
-            SaveManager.instance.saveLevel("Daisy");
+            SaveManager.instance.saveLevel("Tutorial");
             SaveManager.instance.saveName(userName);
             SaveManager.instance.savePlayerPosition(0.0f, 0.0f);
             SaveManager.instance.Save();
--- Game/Assets/Scripts/LevelLoader.cs
+++ Game/Assets/Scripts/LevelLoader.cs
@@ -7,15 +7,6 @@ public class LevelLoader : MonoBehaviour
 {
     public GameObject player;
     public Animator animator;
-    public float transitionTime = 1f;
-
-    public void Start()
-    {
-        //if (SceneManager.GetActiveScene().buildIndex > 0)
-        //{
-        //    playerSpawn();
-        //}
-    }
 
     public void playerRespawn()
     {
@@ -56,28 +47,21 @@ public class LevelLoader : MonoBehaviour
     IEnumerator LoadNextLevel(int index)
     {
         animator.SetTrigger("Start");
-
-        yield return new WaitForSeconds(transitionTime);
-
-
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(index);
     }
 
     public IEnumerator LoadNextLevelString(string level)
     {
         animator.SetTrigger("Start");
-
-        yield return new WaitForSeconds(transitionTime);
-
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(level);
     }
 
     IEnumerator goToMain()
     {
         animator.SetTrigger("Start");
-
         yield return new WaitForSeconds(1.0f);
-      
         SceneManager.LoadScene("MainMenu");
     }
 
